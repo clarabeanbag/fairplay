a8efd2ed500f3c75ef6e662e2e8ce67df846d3f8
Lukebiechu <lukebiechu123@gmail.com>
2024-10-06 10:05:47 +0200

    ScoreManager script

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HouseController : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
+
+    public Text scoreText;
+
+    int score = 0;
+    int totalScore = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
